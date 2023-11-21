6d08c7ba0b1b26451e1502811dd7818bc4581ac0
Henry Gonzalez <hegonza68@gmail.com>
2023-11-21 15:10:48 -0500

    Version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+interface Tarjeta {
+  titulo: string;
+  subtitulo: string;
+  nro?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-first-proyect';
+  title = 'Mi-Primer-Proyecto-Angular';
+  public ArregloTarjetas: Tarjeta[] = [];
+  ngOnInit(): void {
+    this.ArregloTarjetas = [
+      { titulo: 'video 1', subtitulo: 'subtitulo video 1' },
+      { titulo: 'video 2', subtitulo: 'subtitulo video 2' },
+      { titulo: 'video 3', subtitulo: 'subtitulo video 3' },
+    ];
+  }
 }
